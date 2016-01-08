64dac79bfa4d866fc9db7a3b2d69fd81fed6b36f
Bill Shannon <bill@billshannon.com>
2016-01-07 20:55:47 -0700

    setup angular app and controller

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,21 @@
 (function(){
 
-	// script goes here
+	var app = angular.module('myQuiz',[]);
+    app.controller('QuizController', ['$scope', '$http', '$sce', function ($scope, $http, $sce) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }]);
 
 })();
