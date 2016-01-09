119528ec6bf85173cd79a72dd6e1e266cd313d0b
Bill Shannon <bill@billshannon.com>
2016-01-08 20:24:12 -0700

    load json data into app

--- js/quiz.js
+++ js/quiz.js
@@ -3,18 +3,17 @@
 	var app = angular.module('myQuiz',[]);
     app.controller('QuizController', ['$scope', '$http', '$sce', function ($scope, $http, $sce) {
 
+        $scope.score = 0;
+        $scope.activeQuestions = -1;
+        $scope.activeQuestionAnswered = 0;
+        $scope.percentage = 0;
 
 
 
-
-
-
-
-
-
-
-
-
+        $http.get('quiz_data.json').then(function(quizData) {
+            $scope.myQuestions = quizData.data;
+            $scope.totalQuestions = $scope.myQuestions.length;
+        });
 
     }]);
 
